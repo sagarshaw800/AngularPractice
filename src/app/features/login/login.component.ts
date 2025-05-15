98c8392c9f7bd674ffb36a7c0ed2aec7cf147a33
Sagar Shaw <sagarshaw800@gmail.com>
2025-05-15 11:09:57 +0530

    added error messages in login page

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -6,18 +6,28 @@ import { Router } from '@angular/router';
   selector: 'app-login',
   standalone: false,
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
   constructor(private router: Router) {}
 
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', Validators.required)
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{8,}$'),
+    ],),
   });
 
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
   onSubmit() {
+    console.log(this.loginForm.get('password'))
     if (this.loginForm.valid) {
       this.router.navigate(['/dashboard']);
     }
